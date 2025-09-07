1a452eca6a177be2d25794d19ab353244cc6b263
FT51Grupo02 <pfgrupo2ft51@gmail.com>
2025-09-07 16:42:41 -0500

    Avance 07-07-2025#2

--- frontend/src/menu_bar/menu_bar.tsx
+++ frontend/src/menu_bar/menu_bar.tsx
@@ -1,12 +1,12 @@
 function Menu_Bar(){
     return(
-        <nav>
-            <div className="ml-40 mr-40 mt-10 bg-purple-900">
-                <ul className="text-yellow-900">
-                    <li><a>Inicio</a></li>
-                    <li><a>Bebés</a></li>
-                    <li><a>Niños y Niñas</a></li>
-                    <li><a>Adolescentes y Adultos</a></li>
+        <nav className="flex justify-center mt-10 text-4xl">
+            <div className="ml-10 mr-10 mt-10 p-10 bg-purple-900">
+                <ul className="flex space-x-20 text-[rgba(255,215,0,1)]">
+                    <li><a href="#" className="hover">Inicio</a></li>
+                    <li><a href="#" className="hover">Bebés</a></li>
+                    <li><a href="#" className="hover">Niños-Niñas</a></li>
+                    <li><a href="#" className="hover">Adolescentes-Adultos</a></li>
                 </ul>
             </div>
         </nav>
