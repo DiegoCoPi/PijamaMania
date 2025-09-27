691b65a5851a8cebe7f5d2decc2b18cbf150c3b9
FT51Grupo02 <pfgrupo2ft51@gmail.com>
2025-09-27 17:03:21 -0500

    Avance 27-09-2025

--- frontend/src/init_cards/init_cards.tsx
+++ frontend/src/init_cards/init_cards.tsx
@@ -1,17 +1,37 @@
-//import Image from "next/image"
+"use client"
 import {Cards_Interface} from "./cards_interface"
+import {useState} from "react"
 
 function Init_Cards(){
     
-    const Cards_Content:Cards_Interface[]=[
-        {img:"/pijamas/Pijama_Simpson.jpg",name:"Los Simpsons",price:45500, size:"L", stock:20},
-        {img:"/pijamas/Pijama_Mickey_Mouse.jpg",name:"Mickey Mouse",price:45550, size:"M", stock:15},
-        {img:"/pijamas/Pijama_Barcelona.jpg",name:"F.C.Barcelona",price:45550, size:"XL", stock:5},
-    ]
+    const [cards, setCards]=useState<Cards_Interface[]>([
+        {img:"/pijamas/Pijama_Simpson.jpg",name:"Los Simpsons",price:45500, size:"L", quantity:0},
+        {img:"/pijamas/Pijama_Mickey_Mouse.jpg",name:"Mickey Mouse",price:45550, size:"M", quantity:0},
+        {img:"/pijamas/Pijama_Barcelona.jpg",name:"F.C.Barcelona",price:45550, size:"XL", quantity:0},
+    ])
+
+    const incrementClick=(index:number)=>{
+        setCards((prevCards)=>{
+            const newCards =[...prevCards]
+            newCards[index].quantity+=1
+            return newCards
+        })
+    }
+
+    const decrementClick=(index:number)=>{
+        setCards((prevCards)=>{
+            const newCards = [...prevCards]
+            if (newCards[index].quantity>0){
+                newCards[index].quantity-=1
+            }
+            return newCards
+        })
+    }
+    
     
     return(
         <div className="flex justify-center space-x-20 p-10">
-            {Cards_Content.map((card, index)=>(
+            {cards.map((card, index)=>(
                 <div key={index}  className="p-4 rounded-xl shadow-md border border-yellow-400 text-yellow-400 
                      bg-[linear-gradient(180deg,rgba(250,38,160,1)_41%,rgba(33,42,255,1)_82%)] 
                      hover:scale-105 transition-transform">
@@ -23,20 +43,29 @@ function Init_Cards(){
                         <div></div>
                         <br/>
                         
-                        <div className="text-aling text-center">
+                        <div className="flex justify-center space-x-3">
                             <p><strong>Cantidad:</strong></p>
-                            <div>
-                                <img src="/signs/Signo_mas.png" 
-                                     alt="Signo más" width={24} height={24}
-                                     className="bg-[rgba(255,215,0,1)]"
-                                />
-                                <p>{card.stock}</p>
-                                <img src="/signs/Signo_menos.png" 
-                                     alt="Signo menos" width={24} height={24}
-                                     className="bg-[rgba(255,215,0,1)]"
-                                />
+                            <br/>
+                            <div className="flex justify-center space-x-4">
+                                <button onClick={()=>incrementClick(index)} className="rouded-lg">
+                                    <img 
+                                        src="/signs/Signo_mas.png" 
+                                        alt="Signo más" width={24} height={24}
+                                        className="bg-[rgba(255,215,0,1)]"
+                                    />
+                                </button>
+                                <p>{card.quantity}</p>
+                                <button onClick={()=>decrementClick(index)} className="rounded-lg">
+                                    <img src="/signs/Signo_menos.png" 
+                                        alt="Signo menos" width={24} height={24}
+                                        className="bg-[rgba(255,215,0,1)]"
+                                    />
+                                </button>
                             </div>
                         </div>
+                    </div>
+                    <div>
+                        {/*Buy_Button */}
                     </div>
                 </div>
             ))}
@@ -45,3 +74,4 @@ function Init_Cards(){
 }
 
 export default Init_Cards
+
