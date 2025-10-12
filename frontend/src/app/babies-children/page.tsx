51e4de6ba8f09d396242bef2fa3d227e9097f0b4
FT51Grupo02 <pfgrupo2ft51@gmail.com>
2025-10-11 21:41:43 -0500

    Avance 3 11-10-2025

--- frontend/src/app/babies-children/page.tsx
+++ frontend/src/app/babies-children/page.tsx
@@ -5,6 +5,7 @@ import { useState } from "react"
 import Footer from "@/footer/footer"
 import Image from "next/image"
 import Buy_Button from "@/buttons/button_buy"
+import Size_Buttons from "@/sizes/size"
 
 const BabiesChildren= ()=>{
     const [cardBC, setCardBC]=useState<Cards_Interface[]>([
@@ -42,67 +43,107 @@ const BabiesChildren= ()=>{
     }
 
     return(
+    
+    <div>
+        
+        <div className="text-center mt-8">
+            <h1 className="title text-7xl">Bebes-niños y niñas</h1>
+        </div>
+       
         <div>
-            <h1 className="title font-leckerli mt-8">Bebes y Niños</h1>
-            <div>
-                <Menu_Bar/>
-            </div>
-            <br/>
-            <p className="paragraph flex text-left text-lg m-6">
-               En esta sección puede encontrar toda la comodidad, fantasia y ensueño en nuestras pijamas y cobijas
-               para que tu bebe tenga la mejor experiencia en cada sueño desde la comodidad de una prenda hasta el
-               anhelo de verse en la piel de tu personaje favorito.
+            <p className="paragraph m-10 text-2xl">
+                ¡Haz que cada noche sea una aventura! 🌙✨
+                <br/>
+                En esta sección encontrarás pijamas y cobijas llenas de color, suavidad y fantasía, pensadas para 
+                que los más pequeños disfruten de un descanso cómodo, divertido y mágico. Descubre diseños inspirados 
+                en sus personajes favoritos y deja que vivan sus sueños con estilo y comodidad.
+                Explora nuestra colección o usa el buscador si ya tienes algo especial en mente. 💫
             </p>
-            {/*Tarjetas de compras*/}
-            <div className="min-w-[33.3%] text-ailng-center p-4 flex-shrink-0">    
-                <div 
-                    className="flex transition-transform duration-700 ease-in-out"
-                    style={{ transform: `translateX(-${cardMove * (100 / 3)}%)` }}
-                >
-                    {cardBC.map((card,index)=>(
-                        <div key={index} className="p-4 rounded-xl text-yellow-400 bg-[linear-gradient(180deg,rgba(250,38,160,1)_41%,rgba(33,42,255,1)_82%)] 
-                                                    hover:scale-105 transition duration-500">
-                            <Image
-                                src={card.img}
-                                alt={card.name}
-                                height={200}
-                                width={200}
-                                className="object-cover w-full h-[200px] rounded-lg space-x-8"
-                            />
-                            <h2><strong>{card.name}</strong></h2>
-                            <h3>Precio: <strong>${card.price}</strong></h3>
-                            
-                            <div className="flex space-x-1 items-center">
-                                <button onClick={()=>(increment(index))}>
-                                    <Image
-                                        src="/signs/Signo_mas.png"
-                                        alt="signo mas"
-                                        height={20}
-                                        width={20}
-                                        className="bg-[rgba(255,215,0,1)]"
-                                    />
-                                </button>
-                                <h3>{card.quantity}</h3>
-                                <button onClick={()=>(decrement(index))}>
-                                    <Image
-                                        src="/signs/Signo_menos.png"
-                                        alt="signo menos"
-                                        height={20}
-                                        width={20}
-                                        className="bg-[rgba(255,215,0,1)]"
-                                    />
-                                </button>
-                            </div>
-                            <div>
-                                <Buy_Button/>
+        </div>
+        <Menu_Bar/>
+        <br/>
+        <h2 className="text-aling-center title">Catalogo de bebes-niños y niñas</h2>
+        {/*Tarjetas*/}
+        <div className="relative w-full max-w-6xl mx-auto">
+            <div className="flex items-center justify-between">            
+                {/* Botón Izquierda */}
+                <button onClick={prevCard}>
+                    <Image
+                        src="/signs/izquierda.png"
+                        alt="flecha_izquierda"
+                        height={50}
+                        width={50}
+                    />
+                </button>
+                {/* Carrusel */}
+                <div className="overflow-hidden w-[900px]"> 
+                    <div
+                        className="flex transition-transform duration-700 ease-in-out"
+                        style={{ transform: `translateX(-${cardMove * (100 / 3)}%)` }}
+                    >
+                    {cardBC.map((card, index) => (
+                        <div key={index} className="min-w-[33.3%] p-4 flex-shrink-0">
+                            <div className="p-4 rounded-xl text-yellow-400 bg-[linear-gradient(180deg,rgba(250,38,160,1)_41%,rgba(33,42,255,1)_82%)] 
+                                            hover:scale-105 transition duration-500">
+                                <Image
+                                    src={card.img}
+                                    alt={card.name}
+                                    height={200}
+                                    width={200}
+                                    className="object-cover w-full h-[200px] rounded-lg"
+                                />
+                                <h2 className="mt-2 font-bold">{card.name}</h2>
+                                <h3><strong>Talla:</strong> <Size_Buttons /></h3>
+                                <h3><strong>Precio: $</strong>{card.price}</h3>
+                                <div className="flex items-center space-x-2 mt-2">
+                                    <h3><strong>Cantidad:</strong></h3>
+                                    <div className="flex space-x-1 items-center">
+                                        <button onClick={() => increment(index)}>
+                                            <Image
+                                                src="/signs/signo_mas.png"
+                                                alt="signo_mas"
+                                                height={20}
+                                                width={20}
+                                                className="bg-[rgba(255,215,0,1)]"
+                                            />
+                                        </button>
+                                        <h3 className="px-2 bg-purple-900 text-white rounded"><strong>{card.quantity}</strong></h3>
+                                        <button onClick={() => decrement(index)}>
+                                            <Image
+                                                src="/signs/signo_menos.png"
+                                                alt="signo_menos"
+                                                height={20}
+                                                width={20}
+                                                className="bg-[rgba(255,215,0,1)]"
+                                            />
+                                        </button>
+                                    </div>
+                                </div>
+                                <div className="mt-2">
+                                   <Buy_Button />
+                                </div>
                             </div>
                         </div>
                     ))}
                 </div>
             </div>
+        
+            {/* Botón Derecha */}
+                <button onClick={nextCard}>
+                    <Image
+                        src="/signs/derecha.png"
+                        alt="flecha_derecha"
+                        height={50}
+                        width={50}
+                    />
+                </button>
+            </div>
+        </div>
+        <div>
             <Footer/>
         </div>
-        
+    </div>
+
     )
 }
 export default BabiesChildren
