942a29745ebbc8ac7cf3c4d4af1067d6bb207c84
FT51Grupo02 <pfgrupo2ft51@gmail.com>
2025-10-25 18:44:10 -0500

    Avance 25-10-2025

--- frontend/src/app/teenegers-adults/page.tsx
+++ frontend/src/app/teenegers-adults/page.tsx
@@ -1,11 +1,21 @@
 'use client'
 import Cards_Adults_Teenegers from "@/cards/cards_adults_teenegers"
 import Menu_Bar from "@/menu_bar/menu_bar"
+import Footer from "@/footer/footer"
 
 const Teenegers_Adults= ()=>{
     
     return(
         <div>
+            <div className="text-center title text-7xl mt-10">
+                <h1>Adolecentes y adultos</h1>  
+            </div>
+            <div className="paragraph m-10 text-2xl">
+                <p>
+                    En esta sección esta dedicada para los mas grandes y mayores en donde su expesión y estilo pueden reflejarse
+                    en un traje comodo en cada sueño, ya que los diseños son más realistas y sirve para compartir en familia
+                </p>
+            </div>    
             <div>
                 <Menu_Bar/>
             </div>
@@ -13,8 +23,8 @@ const Teenegers_Adults= ()=>{
             <div>
                 <Cards_Adults_Teenegers/>
             </div>
+            <Footer/>
         </div>
-
     )
 }
 export default Teenegers_Adults
